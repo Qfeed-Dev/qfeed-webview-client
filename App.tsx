93fdb7ba716f39eb03446b0b727ca229e499077c
hamo-o <yheel369@gmail.com>
2023-09-01 20:44:06 +0900

    fix: android icon

--- App.tsx
+++ App.tsx
@@ -1,13 +1,9 @@
 import React, {useEffect} from 'react';
 import SplashScreen from 'react-native-splash-screen';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 import {} from 'react-native/Libraries/NewAppScreen';
 import WebViewContainer from './src/components/Webview/WebviewContainer';
 
 function App(): JSX.Element {
-  const Stack = createStackNavigator();
-
   useEffect(() => {
     setTimeout(() => {
       if (SplashScreen) {
@@ -16,39 +12,7 @@ function App(): JSX.Element {
     }, 1000);
   }, []);
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Details"
-        screenOptions={{
-          ...TransitionPresets.SlideFromRightIOS,
-          headerShown: false,
-        }}>
-        <Stack.Screen
-          options={{
-            transitionSpec: {
-              open: {
-                animation: 'spring',
-                config: {
-                  stiffness: 2000,
-                  damping: 1000,
-                },
-              },
-              close: {
-                animation: 'spring',
-                config: {
-                  stiffness: 1000,
-                  damping: 500,
-                },
-              },
-            },
-          }}
-          name="Details"
-          component={WebViewContainer}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <WebViewContainer />;
 }
 
 export default App;
