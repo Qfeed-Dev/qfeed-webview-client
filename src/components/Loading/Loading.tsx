89f947ee036f2a6b72ff9943ee5f170d14affdd7
hamo-o <yheel369@gmail.com>
2023-09-21 07:27:25 +0900

    feat: RN 로딩페이지

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Spinner from 'react-native-loading-spinner-overlay';
+
+const Loading = () => {
+  return <Spinner visible={true} color="#28D3A7" overlayColor="#131313" />;
+};
+
+export default Loading;
--- src/components/Webview/WebviewContent.tsx
+++ src/components/Webview/WebviewContent.tsx
@@ -1,10 +1,12 @@
 import React, {useRef, useState, useEffect} from 'react';
 import {BackHandler} from 'react-native';
 import WebView from 'react-native-webview';
+import Loading from '../Loading/Loading';
 
 const WebViewContent = ({handleClose}: any) => {
   const webview = useRef<any>();
   const [isCanGoBack, setIsCanGoBack] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const backHandler = BackHandler.addEventListener(
@@ -28,19 +30,20 @@ const WebViewContent = ({handleClose}: any) => {
   }, [webview]);
 
   return (
-    <WebView
-      ref={webview}
-      source={{uri: 'https://qfeed-client-web.vercel.app'}}
-      style={{
-        backgroundColor: '#131313',
-      }}
-      pullToRefreshEnabled={true}
-      startInLoadingState={true}
-      allowsBackForwardNavigationGestures={true}
-      mixedContentMode={'compatibility'}
-      originWhitelist={['https://*', 'http://*']}
-      overScrollMode={'never'}
-      injectedJavaScript={`
+    <>
+      <WebView
+        ref={webview}
+        source={{uri: 'https://qfeed-client-web.vercel.app'}}
+        style={{
+          backgroundColor: '#131313',
+        }}
+        pullToRefreshEnabled={true}
+        startInLoadingState={true}
+        allowsBackForwardNavigationGestures={true}
+        mixedContentMode={'compatibility'}
+        originWhitelist={['https://*', 'http://*']}
+        overScrollMode={'never'}
+        injectedJavaScript={`
           (function() {
               function wrap(fn) {
               return function wrapper() {
@@ -57,11 +60,18 @@ const WebViewContent = ({handleClose}: any) => {
           })();
           true;
           `}
-      onMessage={event => {
-        const url = event.nativeEvent.data;
-        setIsCanGoBack(url !== 'https://qfeed-client-web.vercel.app');
-      }}
-    />
+        onMessage={event => {
+          const url = event.nativeEvent.data;
+          setIsCanGoBack(url !== 'https://qfeed-client-web.vercel.app');
+        }}
+        onLoad={() => {
+          setTimeout(() => {
+            setLoading(false);
+          }, 500);
+        }}
+      />
+      {loading ? <Loading /> : null}
+    </>
   );
 };
 
